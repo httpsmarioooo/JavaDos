68f7bbd7af01aa6f0d06b938e5cc7ed416e04a55
Carlos Mario <carlosmariobig14@gmail.com>
2025-05-09 14:18:48 -0500

    update

--- src/com/veterinaria/animales/Perro.java
+++ src/com/veterinaria/animales/Perro.java
@@ -19,40 +19,19 @@ public class Perro {
         this.historialMedico = historialMedico;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public String getRaza() {
-        return raza;
-    }
-
     public void setRaza(String raza) {
         this.raza = raza;
-    }
-
-    public int getEdad() {
-        return edad;
     }
 
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
-    public String getHistorialMedico() {
-        return historialMedico;
-    }
-
     public void setHistorialMedico(String historialMedico) {
         this.historialMedico = historialMedico;
-    }
-
-    public double getPeso() {
-        return peso;
     }
 
     public void setPeso(double peso) {
@@ -60,7 +39,7 @@ public class Perro {
     }
 
     public void mostrarFicha(){
-        System.out.println("Ficha de Registro");
+        System.out.println("Ficha de Registro: ");
         System.out.println("Nombre: "+nombre);
         System.out.println("Raza: "+raza);
         System.out.println("Peso: "+peso);
--- src/com/veterinaria/recepcion/Registro.java
+++ src/com/veterinaria/recepcion/Registro.java
@@ -1,22 +1,24 @@
 package com.veterinaria.recepcion;
 
 import com.veterinaria.animales.Perro;
-
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Registro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Perro> listaPerros = new ArrayList<>();
         int salida = 0;
+
         do {
             Perro perro = new Perro();
             System.out.println("---- MENU VETERINARIA ---- \n" +
-                    "1. Crear perro \n " +
-                    "2. Mostrar perros \n" +
+                    "1. Crear perro\n"+ "2. Mostrar perros \n" +
                     "0. Salir" );
+            System.out.println("Ingrese una opcion: ");
             int opcion = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcion){
                 case 0:
@@ -25,7 +27,6 @@ public class Registro {
                 case 1:
                     System.out.println("Ingrese nombre");
                     perro.setNombre(scanner.nextLine());
-                    scanner.nextLine();
                     System.out.println("Ingrese raza");
                     perro.setRaza(scanner.nextLine());
                     System.out.println("Ingrese la edad");
@@ -50,5 +51,6 @@ public class Registro {
         }while (salida == 0);
     }
 }
+
 
 
